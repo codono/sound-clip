bbf0d3c978cc3ddadb96d42029fbe3d1ea9d0bb4
서덕주 <deokjoo@codonos-MacBookPro.local>
2022-08-03 23:26:11 +0900

    feat SoundHome page

---
+++ client/src/pages/SoundHome.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background-color: #282c34;
+  align-items: center;
+`;
+
+const PageHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  font-size: 40px;
+  color: #ffffff;
+`;
+
+export const SoundHome = () => {
+  return (
+    <Page>
+      <PageHeader></PageHeader>
+    </Page>
+  );
+};
