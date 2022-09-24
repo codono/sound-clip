74efe7f24f1696d021c71b23254f4480aaa2716a
codono <codono@github.com>
2022-09-24 17:23:43 +0900

    add react-lottie lib

--- client/src/pages/SoundHome.tsx
+++ client/src/pages/SoundHome.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import Lottie from 'react-lottie';
+import * as Car from '../assets/10702-car-animation.json';
 
 const Page = styled.div`
   display: flex;
@@ -23,6 +25,18 @@ export const SoundHome = () => {
   return (
     <Page>
       <PageHeader>Title is here</PageHeader>
+      <Lottie
+        options={{
+          loop: true,
+          autoplay: true,
+          animationData: Car,
+          rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+          },
+        }}
+        width={200}
+        height={200}
+      />
     </Page>
   );
 };
